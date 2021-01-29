9355e7e6659e3984aabc300f501afd213030e14f
Issam Mani <issamouu69@gmail.com>
2021-01-29 19:00:04 +0100

    feat: store seed urls in a json file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 const {Crawler} = require('./crawler');
 const {checkFooterYear} = require('./utils');
 
-const startUrls = [
-  'https://www.julieandjessecook.com/',
-  'https://threadexperiment.com/',
-  'https://www.sugarcandybra.com/blog/breast-shapes',
-  'https://uktracking.asendia.com/tracking-dpd.php',
-  'https://threadexperiment.com/', 
-  'https://www.deutschepost.de/sendung/login.html'
-];
-
+// Get seed urls to start crawler
+const startUrls = require('./seed-urls.json').urls;
 
 const crawler = new Crawler({
   startUrls, 
